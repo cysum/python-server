d34539ae4ce6bed9566e14ba55c9eccfb1be99a6
Ben Elgar <me@benelgar.com>
2015-01-29 18:55:38 +0000

    Added basic web API

---
+++ ApiHandler.py
@@ -0,0 +1,48 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from Scanner import Scanner
+import json
+import urllib
+
+class ApiHandler(BaseHTTPRequestHandler):
+    s = Scanner()
+
+    def get_services_json(self, targets, ports):
+        data = self.s.get_services(targets, ports)
+        return json.dumps(data)
+
+
+    def do_GET(self):
+        query = urllib.parse.urlparse(self.path)[4]
+        query_object = urllib.parse.parse_qs(query)
+        print(query_object)
+
+        targets = query_object['targets'][0]
+        ports   = query_object['ports'][0]
+
+        self.send_response(200)
+        self.send_header('Content-type',    'application/json; charset=utf-8')
+        self.end_headers()
+
+        body = self.get_services_json(targets, ports)
+        self.wfile.write(bytes(body, 'utf-8'))
+
+        return
+
+
+
+def main():
+    try:
+        server = HTTPServer(('', 80), ApiHandler)
+        print('Started Server')
+        server.serve_forever()
+        print('still serving')
+    except KeyboardInterrupt:
+        print("^C received - stopping server.")
+        server.socket.close()
+
+
+if __name__ == "__main__":
+    main()
+
+
+
